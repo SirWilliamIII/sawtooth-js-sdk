0e465de22e1ee59e04c3d8e5c661ebdaf6a82576
Will Carpenter <willcarpenteriii@gmail.com>
2018-02-15 20:03:58 -0600

    added compile_protobuf file

--- compile_protobuf.js
+++ compile_protobuf.js
@@ -1,7 +1,7 @@
 /**
  * Copyright 2016 Intel Corporation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,3 +14,35 @@
  * limitations under the License.
  * ------------------------------------------------------------------------------
  */
+
+'use strict'
+
+const protobuf = require('protobufjs')
+const jsonTarget = require('./node_modules/protobufjs/cli/targets/json')
+
+const path = require('path')
+const fs = require('fs')
+
+let root = new protobuf.Root()
+
+const files = fs
+	.readdirSync('../../protos')
+	.map(f => path.resolve('../../protos', f))
+	.filter(f => f.endsWith('.proto'))
+
+try {
+	root = root.loadSync(files)
+} catch (e) {
+	console.log('Unable to load protobuf files!')
+	throw e
+}
+
+jsonTarget(root, {}, (err, output) => {
+	if (err) {
+		throw err
+	}
+
+	if (output !== '') {
+		process.stdout.write(output, 'utf8')
+	}
+})
